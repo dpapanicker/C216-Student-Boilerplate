11844b03de1a14ef757bda2b0d21fbd6dd7d22e2
dpapanicker <70652939+dpapanicker@users.noreply.github.com>
2024-05-23 16:06:07 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,23 @@
+const socket = io("/");
+
+
+
+$(function () { 
+    $("#show_chat").click(function () {
+        $(".left-window").css("display", "none")
+        $(".right-window").css("display", "block")
+        $(".header_back").css("display", "block")
+    })
+    $(".header_back").click(function () {
+        $(".left-window").css("display", "block")
+        $(".right-window").css("display", "none")
+        $(".header_back").css("display", "none")
+    })
+
+ 
+
+   
+
+
+})
+
